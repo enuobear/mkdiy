276f6c06c304e2ed047565a56f94ea658a4c1e6c
enuobear <enuobear@gmail.com>
2014-03-04 17:29:58 +0800

    add files

---
+++ bonus.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(event){
+
+	var bonusList = document.querySelector(".bonus_list")，
+		bonusListBtn = document.querySelector(".bonus_list_click");
+
+	
+	
+}, false);
